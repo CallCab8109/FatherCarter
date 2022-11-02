9c6a9a5ea781bbe8f61d969b0674428f7ff1bf31
Carter Bleyl <Carter.Bleyl@gmail.com>
2022-11-02 12:30:41 -0600

    attempt 1

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const {home} = require("./controllers/pageCtrl.js")
+
+app.get("/", home)
+
+
+const { PORT } = process.env
+
+app.listen(PORT, () => console.log(`Tis listens on the PORT of our lord ${PORT}`))
